142f3921647d387085597ae540366c9de77bc111
Jewel Thomas <jewelat50@gmail.com>
2023-06-16 17:29:31 +0530

    updated file

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def sptext():
         try:
             print("Recognizing....")
             data = recognizer.recognize_google(audio)
-            print(data)
+            #print(data)
+            return data
         except sr.UnknownValueError:
             print("Not Understand")
 
@@ -23,7 +24,36 @@ def speechtx(x):
     voices = engine.getProperty('voices')
     engine.setProperty('voices', voices[0].id)
     rate = engine.getProperty('rate')
-    engine.setProperty('rate',150)
+    engine.setProperty('rate',180)
     engine.say(x)
     engine.runAndWait()
-speechtx("hello guyzz")
+
+if __name__ ==  "__main__":
+
+    data1 = sptext().lower()
+
+    if "your name" in data1:
+        name = "my name is bro"
+        speechtx(name)
+
+    elif "old are you" in data1:
+        age = "i am 20 years old"
+        speechtx(age)
+
+    elif "time" in data1:
+        time = datetime.datetime.now().strftime("%I%M%p")
+        speechtx(time)
+
+    elif 'youtube' in data1:
+        webbrowser.open("https://www.youtube.com/")
+
+    elif 'joke' in data1:
+        jock_1 = pyjokes.get_joke(language="en", category="neutral")
+        speechtx(jock_1)
+
+
+
+
+
+
+
