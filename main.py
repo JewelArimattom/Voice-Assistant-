03670ba243e3c7f05ad8c1595741143a7e48836b
Jewel Thomas <jewelat50@gmail.com>
2023-06-17 13:07:18 +0530

    all set

--- main.py
+++ main.py
@@ -30,26 +30,35 @@ def speechtx(x):
 
 if __name__ ==  "__main__":
 
-    data1 = sptext().lower()
+    while True:
 
-    if "your name" in data1:
-        name = "my name is bro"
-        speechtx(name)
+        data1 = sptext().lower()
 
-    elif "old are you" in data1:
-        age = "i am 20 years old"
-        speechtx(age)
+        if "your name" in data1:
+            name = "my name is bro"
+            speechtx(name)
 
-    elif "time" in data1:
-        time = datetime.datetime.now().strftime("%I%M%p")
-        speechtx(time)
+        elif "old are you" in data1:
+            age = "i am 20 years old"
+            speechtx(age)
 
-    elif 'youtube' in data1:
-        webbrowser.open("https://www.youtube.com/")
+        elif "time" in data1:
+            time = datetime.datetime.now().strftime("%I%M%p")
+            speechtx(time)
 
-    elif 'joke' in data1:
-        jock_1 = pyjokes.get_joke(language="en", category="neutral")
-        speechtx(jock_1)
+        elif 'youtube' in data1:
+            webbrowser.open("https://www.youtube.com/")
+
+        elif 'joke' in data1:
+            jock_1 = pyjokes.get_joke(language="en", category="neutral")
+            speechtx(jock_1)
+
+        elif "exit" in data1:
+            speechtx("thank you")
+            break
+
+
+
 
 
 
